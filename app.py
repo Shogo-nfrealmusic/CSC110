b340518457ffdf206ab547b13192d1aad1ad9ee6
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-24 11:34:35 -0700

    csc110 college class sep24

---
+++ app.py
@@ -0,0 +1,39 @@
+# x=10
+# y=2
+# z=x/y
+# print(z)
+
+# print("x = ",x)
+# print("y = ",y)
+# print("z = ",z)
+
+# a=3
+# b=2
+# print(a, "divided by", b, "=", a/b)
+
+# print("\"What's your idea?\", she asked.")
+# print(int(-75.6))
+
+def inch_to_feet(value):
+    value1 = int(value)
+    feet = value1 // 12  
+    inches = value1 % 12  
+    return f"{feet} feet and {inches} inches"
+
+result = inch_to_feet(33)
+print(result)  
+
+# in_tot=50
+# feet=in_tot//12
+# inches=in_tot%12
+# print(f'{in_tot} in = {feet} feet and {inches} inches')
+
+# print(16-2*5 // 3 + 1)
+
+# print(2**2**3*3)
+
+# print("z =",100 / (2.7*3.1))
+
+# 正しいPythonコード
+z = (7 * (14/3 + 2.3)**2) / (5 - 2**(3/2))**3
+print("z =", round(z,2))
